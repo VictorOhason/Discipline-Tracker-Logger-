459f81da8b44b93766b703bdb3c2e53734c71f8c
NightHavock <victorohason@gmail.com>
2026-04-15 12:34:03 +0100

    feat: implemented saveToday function to handle form submission and localStorage

--- js/script.js
+++ js/script.js
@@ -1,3 +1,55 @@
 document.addEventListener('DOMContentLoaded', function(){
-
+    document.getElementById("save-button").addEventListener("click",saveToday);
+    
 });
+
+function saveToday(){
+    let date = document.getElementById("entry-date").value
+    let pnl = document.getElementById("t-pl").value
+    let trading_rules = document.getElementById("t-rules").value
+    
+    let trading_notes = document.getElementById("notes").value
+    let code_time = document.getElementById("p-hours").value
+    let focus = document.getElementById("d-focus").value
+    
+    let topic = document.getElementById("p-topic").value
+    let wake_time = document.getElementById("d-wake").value
+    let prayer = document.getElementById("s-prayer").value
+    let reflection = document.getElementById("s-reflect").value
+
+    let word_count = document.getElementById("w-count").value
+    let writing_topic = document.getElementById("w-topic").value
+    let self_check = document.getElementById("self-check").value
+    let daily_notes = document.getElementById("misc-notes").value
+    let dated_notes = document.getElementById("misc-date").value
+
+    if (!date || !self_check || !prayer ||!wake_time || !focus){
+        alert("Some fields are missing, please check and fil them in!!")
+        return;
+    }
+
+        let entry ={
+            date: date,
+            pnl: pnl,
+            trading_rules: trading_rules,
+
+            trading_notes: trading_notes,
+            code_time: code_time,
+            topic: topic,
+
+            wake_time: wake_time,
+            prayer: prayer,
+            reflection: reflection,
+
+            word_count: word_count,
+            writing_topic: writing_topic,
+            self_check: self_check,
+
+            daily_notes: daily_notes,
+            dated_notes: dated_notes,
+            focus: focus,
+        }
+        console.log("Saving entry:", entry)
+    localStorage.setItem(date, JSON.stringify(entry))
+    console.log("saved to key:", date)
+};
